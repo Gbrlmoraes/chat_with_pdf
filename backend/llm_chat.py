d1eff33d8adf5daf27c1bae61ea56e18078c4eef
Gabriel Moraes <gabrielmoraesmagalhaes@gmail.com>
2024-10-02 19:03:43 -0300

    Testando comunicação com o GCP

--- backend/llm_chat.py
+++ backend/llm_chat.py
@@ -1,10 +1,7 @@
 from langchain_google_vertexai import ChatVertexAI
-import vertexai
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
-#vertexai.init(project=os.environ['GOOGLE_CLOUD_PROJECT'])
 
 if __name__ == "__main__":
     
