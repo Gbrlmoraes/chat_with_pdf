f3085700b31f3375eaa744f622b85419bb4b2a05
Gabriel Moraes <gabrielmoraesmagalhaes@gmail.com>
2024-10-02 21:27:19 -0300

    iniciando desenvolvimento do backend do chat

--- backend/llm_chat.py
+++ backend/llm_chat.py
@@ -1,15 +1,28 @@
 from langchain_google_vertexai import ChatVertexAI
+from langchain_pinecone import PineconeVectorStore
+from langchain_google_vertexai import VertexAIEmbeddings
+from typing import List, Dict, Any
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-if __name__ == "__main__":
-    
-    llm = ChatVertexAI(
-        model='gemini-1.5-flash',
-        temperature=0,
-        max_retries=0
-    )
 
-    llm_res = llm.invoke('Olá Gemini!')
+def llm_chat(prompt_usuario: str, historico_chat: List[Dict[str, Any]] = []):
+
+    # Definindo o sistema de embeddings, a vector store e qual llm usaremos
+    embeddings = VertexAIEmbeddings(model_name="text-multilingual-embedding-002")
+    vectorstore = PineconeVectorStore(
+        index_name=os.environ.get("INDEX_NAME"), embedding=embeddings
+    )
+    llm = ChatVertexAI(model="gemini-1.5-flash", temperature=0, max_retries=0)
+
+    
+
+
+if __name__ == "__main__":
+
+    llm = ChatVertexAI(model="gemini-1.5-flash", temperature=0, max_retries=0)
+
+    llm_res = llm.invoke("Olá Gemini!")
     print(llm_res.content)
